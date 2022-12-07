366921e9767a2e741180b387c9daf67a3a73a501
BANCHA WAIPEA <banchawaiwai4@gmail.com>
2022-12-07 22:48:32 +0700

    add time

--- examples/time.py
+++ examples/time.py
@@ -1,5 +1,8 @@
 
 from pynmeagps import NMEAReader
+from datetime import datetime
+
+
 file = open('./data_nmea.txt', encoding='utf-8')
 
 for line in file.readlines():
@@ -16,7 +19,13 @@ for line in file.readlines():
         print(msg.msgID) #get the message ID
         print(msg.lat, msg.lon, msg.time) #get the latitude
 
+date_String = "7.12.2022 08:40:12 UTC +0700"
+dt_format = datetime.strptime(date_String, '%d.%m.%Y %H:%M:%S %Z %z')
+print("Date with Timezone Name::", dt_format)
 
+
+timestamp = dt_format.timestamp()
+print("timestamp is::", timestamp)
 # msg = NMEAReader.parse('$GNGGA,083301.00,1358.0162538,N,10026.7303514,E,4,25,0.8,4.006,M,-31.715,M,1.0,0499*72\r\n')
 # print(msg)
 # print(msg.msgID) #get the message ID
